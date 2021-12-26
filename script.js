e67ee051aa5c99d3054c2bc54a29bb83a50c7376
Dylson Oliveira <dylsonuni@gmail.com>
2021-12-26 16:18:34 +0000

    adding more variabeles

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 var searchBtn = document.getElementById("searchBtn");
-var cityNameEl = document.getElementById("#current-city");
 var cityInputEl = document.getElementById("city-input-id");
-
-{/* <h1 id="current-city" class="text-3xl">Atlanta 2021-03-14</h1>
-          <p id="current-temp">Temp: 12 Celcius</p>
-          <p id="current-wind-speed">Wind: 5km/hr</p>
-          <p id="current-humidity">Humidity: 50%</p>
-          <p id="current-uv">UV index: 0.4</p> */}
-
+var currentCityEl = document.getElementById("current-city");
+var currentTemp = document.getElementById("current-temp");
+var currentWindSpeed = document.getElementById("current-wind-speed");
+var currentHumidity = document.getElementById("current-humidity");
+var currentUV = document.getElementById("current-uv");
 
 const APIKey = "7fc61c72acc56d32571bd5733eda5a09";
 
@@ -15,13 +12,16 @@ const getWeather = (cityTerm) => {
   event.preventDefault();
   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityTerm}&appid=${APIKey}`;
   console.log(`This is inside of getWeather:${cityTerm}`);
+
   fetch(queryURL)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
+      currentCityEl.textContent = data.name;
     });
+
 };
 
 searchBtn.addEventListener("click", () => {
